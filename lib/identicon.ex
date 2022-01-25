680016244779fb49deafbd58c9a74ddb736b8a29
Myriam Chevalier <mchevalier@bitfreighter.com>
2022-01-25 13:02:39 -0600

    grab 3 first values from the hex and return color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,8 +2,14 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
+  def pick_color(image) do
+    %Identicon.Image{hex: [r ,g ,b | _]} = image
+
+    %Identicon.Image{image | color: {r, g, b}}
+  end
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
